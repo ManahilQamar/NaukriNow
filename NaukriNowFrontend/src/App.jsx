0b96a7b952efe0df0f2168c2b2e7c3dae3550e0b
Manahil Qamar <manhilqamar@gmail.com>
2026-09-13 08:56:08 -0700

    Fix case-sensitive import for Login component

--- NaukriNowFrontend/src/App.jsx
+++ NaukriNowFrontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
-import Login from "./components/Auth/login";
+import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 import CompanyReviews from "./components/CompanyReviews/CompanyReviews";
 import CompanyDetail from "./components/CompanyDetail/CompanyDetail";
